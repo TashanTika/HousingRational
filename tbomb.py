c7543417f58c16606fb55ebd4becb2ceebfdc5ac
TashanTika <tikatashan@gmail.com>
2020-11-05 23:38:48 +0200

    splitlines

--- tbomb.py
+++ tbomb.py
@@ -10,18 +10,23 @@ driver.get(inp_url)
 
 main_content = driver.find_elements_by_class_name('p24_content')
 
-for info in main_content:    
-    info = main_content[0].text
-    price = info.find_element_by_class_name('p24_price')[0].text
-    prop_title = info.find_elements_by_class_name('p24_title')[0].text
-    location = info.find_elements_by_class_name("p24_location")[0].text
-    description = info.find_elements_by_class_name("p24_excerpt")[0].text
-    feature_details = info.find_elements_by_class_name("p24_featureDetails")[0].text
-    main_rec = pd.DataFrame(data =[[price, prop_title, location, description, feature_details]], columns = ["Price", "Title", "Location", "Description", "Feature Deatails"]) 
+for index, info in enumerate(main_content):    
+    info = main_content[index]
+    price = info.find_element_by_class_name('p24_price').text
+    prop_title = info.find_elements_by_class_name('p24_title')
+    if len(prop_title) > 0:
+        prop_title = info.find_elements_by_class_name('p24_title')[0].text
+        location = info.find_elements_by_class_name("p24_location")[0].text
+        description = info.find_elements_by_class_name("p24_excerpt")[0].text
+        
+        feature_details = info.find_elements_by_class_name("p24_featureDetails")[0].text
+        main_rec = pd.DataFrame(data =[[price, prop_title, location, description, feature_details]], columns = ["Price", "Title", "Location", "Description", "Feature Deatails"]) 
 
 print(info.text)
 
+y = info.text
+z = [list(line) for line in y.splitlines()]
 
-
+x = y.splitlines(True)
 
 
