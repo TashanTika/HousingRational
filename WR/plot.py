a96ab0ef013fa3e1044bae5be4fab523d4cfddb8
NeeradjGangapersad <neeradj.gangapersad@gmail.com>
2020-11-14 17:37:37 +0100

    finished first basic plot of listings

--- WR/plot.py
+++ WR/plot.py
@@ -27,12 +27,31 @@ if removed_rec > 0:
 
 # plot basic data
 logging.info("plotting listings")
-import plotly.express as px
 from plotly.offline import plot
-px.set_mapbox_access_token(conf.mapbox_token)
-fig = px.scatter_mapbox(sta_df,
-                        lat = "lat",
-                        lon = "lon",
+import plotly.graph_objects as go
+
+# TO DO refine marker color based on price and add more info in text box (Title, address, size etc.) like here https://docs.mapbox.com/mapbox-gl-js/example/popup-on-hover/
+scatt = go.Scattermapbox(
+                        lat = list(sta_df["lat"]),
+                        lon = list(sta_df["lon"]),                        
+                        mode='markers',
+                        hoverinfo='text',
+                        marker=dict(symbol ='marker', size=5, color='blue'),
+                        textposition='top right',
+                        textfont=dict(size=16, color='black'),
+                        text= list(sta_df["Address"])
                         )
-fig.show()
+
+layout = go.Layout(hovermode = "closest",
+                    mapbox = dict(           
+                                 accesstoken= conf.mapbox_token,
+                                 zoom=8,
+                                 center=dict(lat = sta_df["lat"].median(),
+                                             lon = sta_df["lon"].median()
+                                             )
+                               )
+                    )
+
+            
+fig=go.Figure(data = [scatt], layout = layout)
 plot(fig)
