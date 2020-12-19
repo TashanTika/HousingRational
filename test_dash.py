4b771d5b0789323c25b6f3f7124775ec7f5368a9
TashanTika <tikatashan@gmail.com>
2020-12-19 20:13:21 +0200

    19/12/2020

--- test_dash.py
+++ test_dash.py
@@ -1,46 +1,46 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import pandas as pd
-import plotly.graph_objs as go
-import config_p24
-import plotly.express as px
+# import dash
+# import dash_core_components as dcc
+# import dash_html_components as html
+# from dash.dependencies import Input, Output
+# import pandas as pd
+# import plotly.graph_objs as go
+# import config_p24
+# import plotly.express as px
 
-mapbox_token = (config_p24.mapbox_token)
-df = pd.read_excel(config_p24.dash_ready)
-selected_location = "Morningside"
-location = df["Location"].drop_duplicates()
-app = dash.Dash()
-
-
-app.layout = html.Div([
-                        dcc.Graph(id="graph"),
-                        dcc.Dropdown(id="type_picker",
-                        options=[
-                            {'label': 'Morningside', 'value': 'Morningside'},
-                            {'label': 'Esplanade', 'value': 'Esplanade'},
-                        ],
-                        value='Morningside',)
+# mapbox_token = (config_p24.mapbox_token)
+# df = pd.read_excel(config_p24.dash_ready)
+# selected_location = "Morningside"
+# location = df["Location"].drop_duplicates()
+# app = dash.Dash()
 
 
-])
+# app.layout = html.Div([
+#                         dcc.Graph(id="graph"),
+#                         dcc.Dropdown(id="type_picker",
+#                         options=[
+#                             {'label': 'Morningside', 'value': 'Morningside'},
+#                             {'label': 'Esplanade', 'value': 'Esplanade'},
+#                         ],
+#                         value='Morningside',)
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
 
-#make the call back work
+# ])
 
-#use map    
-@app.callback(Output('graph', 'figure'),
-              [Input('type_picker', 'value')])
-def update_figure(selected_location):    
-    df_new = df.loc[(df["Location"] == selected_location)]
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
+
+# #make the call back work
+
+# #use map    
+# @app.callback(Output('graph', 'figure'),
+#               [Input('type_picker', 'value')])
+# def update_figure(selected_location):    
+#     df_new = df.loc[(df["Location"] == selected_location)]
     
-    fig = px.scatter(
-        df_new, x="Price", y="Size",  
-        hover_data=['Address'])
-    return fig
+#     fig = px.scatter(
+#         df_new, x="Price", y="Size",  
+#         hover_data=['Address'])
+#     return fig
     
     
     
@@ -83,3 +83,125 @@ def update_figure(selected_location):
     # fig = dict(data=data, layout=layout)    
 
     # return fig
+    
+    
+ #From pull reuest fixed by neer !!   
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.graph_objs as go
+import config_p24
+import plotly.express as px
+
+mapbox_token = (config_p24.mapbox_token)
+df = pd.read_excel(config_p24.dash_ready)
+app = dash.Dash()
+
+
+app.layout = html.Div([
+                        dcc.Dropdown(id="location_picker",                                                                                                                 
+                                    options=[{'label': i, 'value': i} for i in df["Location"].unique()], #fixed this using https://community.plotly.com/t/how-to-populate-a-dropdown-from-unique-values-in-a-pandas-data-frame/5543/2
+                                    value='Morningside',),
+                        dcc.Graph(id="map"),
+                        dcc.Graph(id="scatter"),
+
+])
+
+
+#make the call back work
+@app.callback(Output('scatter', 'figure'),
+              [Input('location_picker', 'value')])
+def update_scatter(selected_location):    
+    df_new = df.loc[(df["Location"] == selected_location)]
+    
+    fig = px.scatter(
+        df_new, x="Price", y="Size",  
+        hover_data=['Address'])
+    return fig
+
+#use map    
+@app.callback(Output('map', 'figure'),
+              [Input('location_picker', 'value')])
+def update_map(selected_location):    
+    df_new = df.loc[(df["Location"] == selected_location)]
+    
+    data = [go.Scattermapbox(lat=df_new["lat"], 
+                              lon=df_new["lon"],
+                              mode="markers", 
+                              marker=dict(size=8),
+                              text=df_new["Address"])]
+    
+    layout = go.Layout(autosize=True, 
+                        hovermode="closest", 
+                        mapbox=dict(bearing=0,pitch=0, zoom=30, accesstoken=mapbox_token,),
+                        mapbox_style="open-street-map")
+    
+    fig = dict(data=data, layout=layout)    
+    return fig
+    
+if __name__ == '__main__':
+    app.run_server(debug=False)    
+    
+   
+#---------------------------------------------------------------------------------------------
+#My Try 
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.graph_objs as go
+import config_p24
+import plotly.express as px
+
+mapbox_token = (config_p24.mapbox_token)
+df = pd.read_excel(config_p24.dash_ready)
+app = dash.Dash()
+
+
+app.layout = html.Div([
+                        dcc.Dropdown(id="location_picker",                                                                                                                 
+                                    options=[{'label': i, 'value': i} for i in df["Location"].unique()], #fixed this using https://community.plotly.com/t/how-to-populate-a-dropdown-from-unique-values-in-a-pandas-data-frame/5543/2
+                                    value=input(""),),
+                        dcc.Graph(id="map"),
+                        dcc.Graph(id="scatter"),
+
+])
+
+
+#make the call back work
+@app.callback(Output('scatter', 'figure'),
+              [Input('location_picker', 'value')])
+def update_scatter(selected_location):    
+    df_new = df.loc[(df["Location"] == selected_location)]
+    
+    fig = px.scatter(
+        df_new, x="Price", y="Size",  
+        hover_data=['Address'])
+    return fig
+
+#use map    
+@app.callback(Output('map', 'figure'),
+              [Input('location_picker', 'value')])
+def update_map(selected_location):    
+    df_new = df.loc[(df["Location"] == selected_location)]
+    
+    data = [go.Scattermapbox(lat=df_new["lat"], 
+                              lon=df_new["lon"],
+                              mode="markers", 
+                              textfont=dict(size=16, color='black'),
+                              marker=dict(symbol ='marker', size=5, color='white'),
+                              text=df_new["Address"])]
+    
+    layout = go.Layout(autosize=True, 
+                        hovermode="closest", 
+                        mapbox=dict(bearing=0,pitch=0, zoom=30, accesstoken=mapbox_token,),
+                        mapbox_style="open-street-map")
+    
+    fig = dict(data=data, layout=layout)    
+    return fig
+    
+if __name__ == '__main__':
+    app.run_server(debug=False)    
